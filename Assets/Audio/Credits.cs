e544b961992b5622d2be536fc2f60d4751c3bb14
Lunalavendermoon <lunalavendermoon@gmail.com>
2025-05-27 15:55:35 -0700

    added audio + memory gained ui

---
+++ Assets/Audio/Credits.cs
@@ -0,0 +1,7 @@
+/**
+main: https://pixabay.com/music/meditationspiritual-ethereal-dreams-vol1-ethereal-stillness-280427/
+cutscene: https://pixabay.com/music/ambient-ethereal-ambient-sound-for-film-323495/ 
+badEnd: https://pixabay.com/music/ambient-dark-intentions-288498/ 
+echoclick: https://freesound.org/people/BaggoNotes/sounds/721503/ 
+echoding: https://freesound.org/people/Divinux/sounds/198412/ 
+**/
