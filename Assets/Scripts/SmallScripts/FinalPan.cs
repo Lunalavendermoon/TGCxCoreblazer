6a52290f147132cbde2d6a798517859cd3b19f4f
Yuwen Zhang <yuwen.x.zhang@gmail.com>
2025-06-07 12:44:47 -0700

    added final pan, house collider

---
+++ Assets/Scripts/SmallScripts/FinalPan.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class FinalPan : MonoBehaviour
+{
+    [SerializeField] GameObject finalPanParent;
+    [SerializeField] GameObject playerCamera;
+    [SerializeField] GameObject player;
+
+    private void Start()
+    {
+        //startPan();
+    }
+    public void startPan()
+    {
+        StartCoroutine(playPanningAnimation());
+    }
+    private IEnumerator playPanningAnimation()
+    {
+        player.transform.position = new Vector3(54.39f, -6.66f, -51.05f);
+        playerCamera.SetActive(false);
+
+        GameObject prevCamera = null;
+        foreach (Transform camera in finalPanParent.transform)
+        {
+            GameObject cameraObject = camera.gameObject;
+            //Debug.Log(cameraObject.name + " activated");
+
+            cameraObject.SetActive(true);
+            if(prevCamera)
+            {
+                prevCamera.SetActive(false);
+            }
+            prevCamera = cameraObject;
+            yield return new WaitForSeconds(2f);
+        }
+
+        //playerCamera.SetActive(true);
+        //prevCamera.SetActive(false);
+    }
+}
